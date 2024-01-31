3eb5eef8fca2be6aacf297d8b3cf815010522429
akhilk49 <akhilkkurian49@gmail.com>
2024-01-31 17:03:45 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
-import './App.css';
-import LikeImage from './components/LikeImage';
-import LikePost from './components/LikePost';
+import "./App.css";
+import LikeImage from "./components/LikeImage";
+import LikePost from "./components/LikePost";
+import HigherOrderComponent from "./components/HigherOrderComponent//HigherOrderComponent";
+const LikePostH = HigherOrderComponent(LikeImage);
+const LikeImageH = HigherOrderComponent(LikePost);
+
+import RenderPropsComponent from "./components/RenderProps/RenderPropsComponent";
+import LikeImageRender from "./components/RenderProps/LikeImageRender";
+import LikePostRender from "./components/RenderProps//LikePostRender";
 
 function App() {
   return (
-    <div>
-      <h3>Some Blog</h3>
-      <div className='buttons'>
-        <LikePost/>
-        <LikeImage/>
+    <div className="parent">
+      <h3>Normal Components</h3>
+      <div className="buttons">
+        <LikePost />
+        <LikeImage />
+      </div>
+      <h3>HOC</h3>
+      <div className="hof buttons">
+        <LikePostH />
+        <LikeImageH />
+      </div>
+      <h3>Render Props</h3>
+      <div className="render buttons">
+        <RenderPropsComponent
+          render={(count, incrementCount) => (
+            <LikeImageRender count={count} incrementCount={incrementCount} />
+          )}
+        />
+        <RenderPropsComponent
+          render={(count, incrementCount) => (
+            <LikePostRender count={count} incrementCount={incrementCount} />
+          )}
+        />
       </div>
     </div>
   );
--- src/components/LikePost.jsx
+++ src/components/LikePost.jsx
@@ -1,16 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import '../App.css'
 
 export default function LikePost() {
-
   const [likePostCounter, setPostCounter] = useState(0);
 
-  const handlePostCount = ()=>{
-    setPostCounter(likePostCounter+1);
-  }
+  const handlePostCount = () => {
+    setPostCounter(likePostCounter + 1);
+  };
 
   return (
     <div>
       <button onClick={handlePostCount}>Like Post {likePostCounter}</button>
     </div>
-  )
+  );
 }
